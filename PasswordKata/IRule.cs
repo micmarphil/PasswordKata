69763e92b70750b910e9970a4aab994bf8e5c364
Michael Phillips <michael.m.phillips@sherwin.com>
2016-07-27 09:46:52 -0400

    Reinstate other files after removing DG files

---
+++ PasswordKata/IRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordKata
+{
+    public interface IRule
+    {
+        bool Validate(string password);
+        string FailureMessage();
+    }
+}
